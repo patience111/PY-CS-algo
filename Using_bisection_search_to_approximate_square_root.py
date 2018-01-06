db7baa0c82fdd42e2dec019dd46d5cbf8a39942f
Yao Pei <scpeiyao@gmail.com>
2018-01-06 16:04:02 +0800

    mend nagative situation

--- Using_bisection_search_to_approximate_square_root.py
+++ Using_bisection_search_to_approximate_square_root.py
@@ -1,17 +1,19 @@
-x=25
+x=-25
 epsilon=0.01
 numGuesses=0
 low=0.0
-high=max(1.0,x)
+high=max(1.0,abs(x))
 ans=(high+low)/2.0
-while abs(ans**2-x)>=epsilon:
+while abs(ans**2-abs(x))>=epsilon:
     print('low=', low,'high=',high,'ans=',ans)
     numGuesses+=1
-    if ans**2<x:
+    if ans**2<abs(x):
         low=ans
     else:
         high=ans
     ans=(high+low)/2.0
 print('numGuesses=',numGuesses)
-print(ans,'is close to square root of',x)
-
+if x>0:
+    print(ans,'is close to square root of',x)
+else:
+    print(-ans,'is close to square root of',x)
