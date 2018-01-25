020733e473895b466e77c5a3c27786ba381b3337
Yao Pei <scpeiyao@gmail.com>
2018-01-25 12:33:52 +0800

    add circle module

---
+++ circles.py
@@ -0,0 +1,10 @@
+pi=3.14159
+def area(radius):
+    return pi*(radius**2)
+def circumference(radius):
+    return 2*pi*radius
+def spheresurface(radius):
+    return 4.0*area(radius)
+def spherevolumn(radius):
+    return (4.0/3.0)*pi*(radius**3)
+
