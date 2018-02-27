704e7af5ef8c19adca14bbab951aeb05305c5c71
Yao Pei <scpeiyao@gmail.com>
2018-02-27 19:55:43 +0800

    read odometer

--- car.py
+++ car.py
@@ -3,8 +3,14 @@ class Car():
         self.make=make
         self.model=model
         self.year=year
+        self.odometer_reading=0
     def descriptive_name(self):
         long_name=str(self.year)+' '+self.make+' '+self.model
         return long_name.title()
+    def read_odometer(self):
+        print('This car has '+str(self.odometer_reading)+" moles on it.")
+
 my_new_car=Car('audi','a4',2016)
 print(my_new_car.descriptive_name())
+my_new_car.read_odometer()
+
