60399b03ea23974a55f7e39e2d33c8f629c1c8b3
Yao Pei <scpeiyao@gmail.com>
2018-02-27 17:26:48 +0800

    instance dog

--- dog.py
+++ dog.py
@@ -1,4 +1,4 @@
-class dog():
+class Dog():
     '''a simple attempt to model a dog'''
     def __init__(self,name,age):
         self.name=name
@@ -7,4 +7,6 @@ class dog():
         print(self.name.title()+'is now sitting.')
     def roll_over(self):
         print(self.name.title()+'rolled over!')
-
+my_dog=Dog('willie',6)
+print("my dog's name is"+my_dog.name.title()+'.')
+print('my dog is '+str(my_dog.age)+' years old.')
