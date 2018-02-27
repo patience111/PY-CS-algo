91c3e3343b610b62e63db38165948d4d798a5733
Yao Pei <scpeiyao@gmail.com>
2018-02-27 19:44:27 +0800

    car class

--- dog.py
+++ dog.py
@@ -10,3 +10,9 @@ class Dog():
 my_dog=Dog('willie',6)
 print("my dog's name is"+my_dog.name.title()+'.')
 print('my dog is '+str(my_dog.age)+' years old.')
+your_dog=Dog('Lucy',3)
+my_dog.sit()
+print("\nYour dog's name is "+your_dog.name.title()+'.')
+print("Your dog is "+str(your_dog.age)+' years old.')
+your_dog.sit()
+
