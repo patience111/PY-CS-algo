278e3bf4efc3717802881837b9826eaca55fbd1d
Yao Pei <scpeiyao@gmail.com>
2018-02-28 10:19:21 +0800

    import multi class from one module

---
+++ my_cars.py
@@ -0,0 +1,5 @@
+from car import Car,ElectricCar
+my_beetle=Car('volkswagen','beetle',2016)
+print(my_beetle.descriptive_name())
+my_tesla=ElectricCar('tesla','model s',2016)
+print(my_tesla.descriptive_name())
