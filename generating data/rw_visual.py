b0c8a7d2135d45bedd1188ee325109a626fe760d
Yao Pei <scpeiyao@gmail.com>
2018-04-09 16:19:50 +0800

    random walk visulization

---
+++ generating data/rw_visual.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+# make a random walk, and plot the points
+# keep making new walks, as long as the program is active
+# while True:
+rw = RandomWalk()
+rw.fill_walk()
+# set the size of the plotting window
+plt.figure(figsize=(10, 6), dpi=128)
+point_numbers = list(range(rw.num_points))
+plt.plot(rw.x_values, rw.y_values, linewidth=1)
+#plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+# emphasize the first and last points
+plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+plt.axes().get_xaxis().set_visible(False)
+plt.axes().get_yaxis().set_visible(False)
+plt.show()
+#keep_running = input('Make another walk? (y/n):')
+# if keep_running == 'n':
+#   break
