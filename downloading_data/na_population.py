8cef423d3d5f91e7804d92cdc12da6f751c521b7
Yao Pei <scpeiyao@gmail.com>
2018-05-11 13:54:32 +0800

    map for population of north america

---
+++ downloading_data/na_population.py
@@ -0,0 +1,5 @@
+import pygal
+wm = pygal.maps.world.World()
+wm.title = 'Populations of Countries in North America'
+wm.add('North America', {'ca': 34126000, 'us': 309349000, 'mx': 113243000})
+wm.render_to_file('na.populations.svg')
