2c3a31ea6fecc1218ef1962bb739c52c93ff3367
Yao Pei <scpeiyao@gmail.com>
2018-05-12 16:18:31 +0800

    group and styled

--- downloading_data/world_population.py
+++ downloading_data/world_population.py
@@ -1,6 +1,7 @@
 from country_codes import get_country_code
 import json
 import pygal
+from pygal.style import RotateStyle as rs, LightColorizedStyle as lcs
 # load the data into a list
 with open('population_data.json') as f:
     pop_data = json.load(f)
@@ -18,7 +19,22 @@ for pop_dict in pop_data:
             #print(code + ':' + ' ' + str(population))
         # else:
             #print('ERROR - ' + country_name)
-wm = pygal.maps.world.World()
+# group the countries into 3 population level
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_population.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+# see how many countries are in each level
+#print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+wm_style = rs('#336699', base_style=lcs)
+wm = pygal.maps.world.World(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_population)
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
 wm.render_to_file('world_population.svg')
